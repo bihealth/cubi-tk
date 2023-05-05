7810453eb42f025f52e8367254c28b0bb4c0e589
Thomas Sell <thomas.sell@charite.de>
2023-05-05 16:00:07 +0200

    changed to nbsp

--- cubi_tk/common.py
+++ cubi_tk/common.py
@@ -195,9 +195,9 @@ def sizeof_fmt(num, suffix="B"):  # pragma: nocover
     """Source: https://stackoverflow.com/a/1094933/84349"""
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
-            return "%3.1f %s%s" % (num, unit, suffix)
+            return "%3.1f %s%s" % (num, unit, suffix)
         num /= 1024.0
-    return "%.1f %s%s" % (num, "Yi", suffix)
+    return "%.1f %s%s" % (num, "Yi", suffix)
 
 
 def get_terminal_columns():
