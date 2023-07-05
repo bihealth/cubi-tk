26748701f2d6c5db5cb50a3b481713ca1d3d4272
Max Zhao <max.zhao@arsbrevis.de>
2023-07-05 18:03:41 +0200

    Adding rudimentary test for snappy_workflows

---
+++ tests/test_snappy_workflows.py
@@ -0,0 +1,22 @@
+"""Tests for ``cubi_tk.snappy.snappy_workflows``.
+"""
+
+
+from typing import Callable
+import pytest
+
+from cubi_tk.snappy.snappy_workflows import SnappyWorkflowManager
+
+from .hide_modules import hide_modules
+
+
+@hide_modules(["snappy_pipeline"])
+def test_could_not_import_module():
+    manager = SnappyWorkflowManager.from_snappy()
+    assert manager is None
+
+def test_could_import_module():
+    manager = SnappyWorkflowManager.from_snappy()
+    assert manager is not None
+    assert callable(manager._expand_ref)
+    assert len(manager._step_to_module.keys()) > 0
