610f70b36720ab80ce5ba13f60173a7dbb9ce342
Thomas Sell <thomas.sell@charite.de>
2023-09-18 15:06:24 +0200

    add run_cookiecutter smoketest

--- tests/test_isa_tpl.py
+++ tests/test_isa_tpl.py
@@ -3,11 +3,22 @@
 from argparse import ArgumentParser
 from unittest.mock import patch
 
-from cubi_tk.isa_tpl.__init__ import validate_output_directory
+from cubi_tk.isa_tpl.__init__ import run_cookiecutter, validate_output_directory
+from cubi_isa_templates import TEMPLATES
+
+
+def test_run_cookiecutter(tmp_path):
+    tpl = TEMPLATES["generic"]
+    args = type('test', (), {})()
+    path = tmp_path / "dir"
+    args.output_dir = str(path)
+    args.verbose = False
+
+    run_cookiecutter(tpl, args, no_input = True)
 
 
 @patch.object(ArgumentParser, "error")
-def test_validate_output_directory(test_error, tmp_path):
+def test_validate_output_directory(mockerror, tmp_path):
     parser = ArgumentParser()
     d = tmp_path / "dir"
     d.mkdir()
@@ -15,4 +26,4 @@ def test_validate_output_directory(test_error, tmp_path):
     validate_output_directory(parser, d)
     validate_output_directory(parser, d2)
     validate_output_directory(parser, d2.parent)
-    assert parser.error.call_count == 2
+    assert mockerror.call_count == 2
